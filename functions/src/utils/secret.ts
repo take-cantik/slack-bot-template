e3e893f38cb9b1b73ab21e618837f0fc47f47f4e
take-cantik <take.cantik17@gmail.com>
2022-05-05 18:14:04 +0900

    Update: fix env

--- functions/src/utils/secret.ts
+++ functions/src/utils/secret.ts
@@ -1,6 +1,6 @@
 // slack
 
-export const SLACK_ACCESS_TOKEN = process.env.SLACK_ACCESS_TOKEN as string
-export const SLACK_SIGNIN_SECRET = process.env.SLACK_SIGNIN_SECRET as string
-export const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID as string
-export const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET as string
+export const SLACK_ACCESS_TOKEN = process.env.SLACK_ACCESS_TOKEN
+export const SLACK_SIGNIN_SECRET = process.env.SLACK_SIGNIN_SECRET
+export const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID
+export const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET
--- functions/src/utils/slack.ts
+++ functions/src/utils/slack.ts
@@ -3,32 +3,27 @@ import { logger } from 'firebase-functions/v1'
 import { SLACK_ACCESS_TOKEN, SLACK_SIGNIN_SECRET } from './secret'
 
 export const expressReceiver = new ExpressReceiver({
-  signingSecret: SLACK_SIGNIN_SECRET,
+  signingSecret: SLACK_SIGNIN_SECRET || '',
   endpoints: '/events',
   processBeforeResponse: true
 })
 
 const app = new App({
   receiver: expressReceiver,
-  token: SLACK_ACCESS_TOKEN
+  token: SLACK_ACCESS_TOKEN || ''
 })
 
 // test
+app.command('/test', async ({ ack, body, context }) => {
+  await ack()
 
-const useTest = (app: App) => {
-  app.command('/test', async ({ ack, body, context }) => {
-    await ack()
-
-    try {
-      await app.client.chat.postMessage({
-        token: context.botToken,
-        channel: body.channel_id,
-        text: 'test'
-      })
-    } catch (err) {
-      logger.log(err)
-    }
-  })
-}
-
-useTest(app)
+  try {
+    await app.client.chat.postMessage({
+      token: context.botToken,
+      channel: body.channel_id,
+      text: 'test'
+    })
+  } catch (err) {
+    logger.log(err)
+  }
+})
