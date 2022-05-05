4a62168e558641a58d424b74dd5226331eb3ff3a
take-cantik <take.cantik17@gmail.com>
2022-05-05 19:26:37 +0900

    Update: create useTest

--- functions/src/triggers/https/slack-bot/handlers/command.ts
+++ functions/src/triggers/https/slack-bot/handlers/command.ts
@@ -1,16 +1,18 @@
-import { app } from '~/utils/slack'
 import { logger } from 'firebase-functions'
+import { App } from '@slack/bolt'
 
-app.command('/test', async ({ ack, body, context }) => {
-  await ack()
+export const useTest = (app: App) => {
+  app.command('/test', async ({ ack, body, context }) => {
+    await ack()
 
-  try {
-    await app.client.chat.postMessage({
-      token: context.botToken,
-      channel: body.channel_id,
-      text: 'test'
-    })
-  } catch (err) {
-    logger.log(err)
-  }
-})
+    try {
+      await app.client.chat.postMessage({
+        token: context.botToken,
+        channel: body.channel_id,
+        text: 'test'
+      })
+    } catch (err) {
+      logger.log(err)
+    }
+  })
+}
--- functions/src/utils/slack.ts
+++ functions/src/utils/slack.ts
@@ -1,4 +1,5 @@
 import { App, ExpressReceiver } from '@slack/bolt'
+import { useTest } from '~/triggers/https/slack-bot/handlers/command'
 import { SLACK_ACCESS_TOKEN, SLACK_SIGNIN_SECRET } from './secret'
 
 export const expressReceiver = new ExpressReceiver({
@@ -11,3 +12,5 @@ export const app = new App({
   receiver: expressReceiver,
   token: SLACK_ACCESS_TOKEN || ''
 })
+
+useTest(app)
